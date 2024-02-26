d76e111d20021b41ad9cd36e358959ae162bf425
Angel-Dev-Luna <147888279+Angel-Dev-Luna@users.noreply.github.com>
2024-02-26 09:08:02 -0600

    Patron esteuctural base de datos

--- js/controladore/firebase.js
+++ js/controladore/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { GoogleAuthProvider  } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,16 +19,21 @@ const firebaseConfig = {
   measurementId: "G-9NBGXFH8L1"
 };
 
-// Initialize Firebase
+
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-  
-      // Referencia a la base de datos en tiempo real
-      const dbRef = firebase.database().ref("videos");
-  
-      // Escuchar cambios en la base de datos
-      dbRef.on("value", (snapshot) => {
-        const videos = snapshot.val();
-        console.log("Nuevos cambios en la base de datos:");
-        console.log(videos);
-      });
+const database = getDatabase(app);
+
+// Función para configurar la escucha en tiempo real en la base de datos
+function configurarEscuchaEnTiempoReal() {
+  const dbRef = ref(database, "videos");
+  onValue(dbRef, (snapshot) => {
+    const videos = snapshot.val();
+    console.log("Nuevos cambios en la base de datos:");
+    console.log(videos);
+  });
+}
+
+// Llamar a la función para configurar la escucha en tiempo real
+configurarEscuchaEnTiempoReal();
